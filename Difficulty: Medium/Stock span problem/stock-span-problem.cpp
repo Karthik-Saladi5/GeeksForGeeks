ddd7860b6b16adb36fcb539fb8f0685480f59e78
Saladi Sri Sai Karthik <119729884+Karthik-Saladi5@users.noreply.github.com>
2026-01-21 23:02:05 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Stock span problem/stock-span-problem.cpp
@@ -0,0 +1,16 @@
+class Solution {
+  public:
+    vector<int> calculateSpan(vector<int>& arr) {
+        // code here
+        int n = arr.size();
+        vector<int> ans(n,0);
+        stack<int> st;
+        for(int i=0; i<n; i++){
+            while(st.size() > 0 && arr[st.top()] <= arr[i]) st.pop();
+            if(st.size() == 0) ans[i] = i+1;
+            else ans[i] = i - st.top();   
+            st.push(i);
+        }
+        return ans;
+    }
+};
